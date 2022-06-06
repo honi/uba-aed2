42ac7bc1be658a478589c1bbd5c99bc592c7e1a7
Joni Bekenstein <joni@bek.io>
2022-06-06 10:07:30 -0300

    Arreglo ejercicio 17 y separa las soluciones en archivos tex individuales

--- Prácticas/Soluciones/Práctica5/python/ej17.py
+++ Prácticas/Soluciones/Práctica5/python/ej17.py
@@ -4,11 +4,11 @@ from utils import CountingSort
 
 def OrdenarConSwaps(A: List[int]) -> List[int]:
     n = len(A)
-    for i in range(n - 1):
-        if A[i] > A[i + 1]:
+    for i in range(n - 1, 0, -1):
+        if A[i] < A[i - 1]:
             tmp = A[i]
-            A[i] = A[i + 1]
-            A[i + 1] = tmp
+            A[i] = A[i - 1]
+            A[i - 1] = tmp
     return A
 
 def runTestCase(A: List[int]):
@@ -19,3 +19,5 @@ if __name__ == "__main__":
     runTestCase([1, 2, 3, 4, 5])
     runTestCase([2, 1, 4, 3, 5])
     runTestCase([1, 5, 4, 7, 6])
+    runTestCase([1, 3, 4, 2, 7])
+    runTestCase([1, 2, 3, 5, 4])
